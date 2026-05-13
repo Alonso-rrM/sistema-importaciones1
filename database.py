85627e442e74c51cbd37f3634a03b667648388d4
Alonso-rrM <lma59072@gmail.com>
2026-05-13 12:14:00 -0500

    Update database.py
    fin de la prueba volvemos al original

--- database.py
+++ database.py
@@ -8,4 +8,3 @@ URL_BASE_DATOS = "postgresql://postgres:Dmc081953@localhost:5432/postgres"
 engine = create_engine(URL_BASE_DATOS)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-# para victor
