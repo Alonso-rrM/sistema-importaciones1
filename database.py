4df6fe98f4f5a0d09e5a45fd867a4e292ec51b9f
Alonso-rrM <lma59072@gmail.com>
2026-05-14 11:53:38 -0500

    cambio de nombre de base de datos
    Se transladaron las tablas de postgres al importaciones db

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Cambia "TU_CONTRASEÑA" por la clave de tu usuario postgres
-URL_BASE_DATOS = "postgresql://postgres:Dmc081953@localhost:5432/postgres"
+URL_BASE_DATOS = "postgresql://postgres:Dmc081953@localhost:5432/importaciones_db"
 
 engine = create_engine(URL_BASE_DATOS)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
